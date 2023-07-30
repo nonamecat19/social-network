e226fe602356466d3049460b0e0237ef698a1439
Каріна Збунь <vt211_zks@student.ztu.edu.ua>
2023-07-30 20:50:04 +0300

    basket2

--- server/src/modules/basket/basket.service.ts
+++ server/src/modules/basket/basket.service.ts
@@ -6,16 +6,12 @@ import { BasketEditInput } from '../../graphql-input/basket-edit.input'
 import { BasketAddInput } from '../../graphql-input/basket-add.input'
 import { BasketEntity } from '../../types/basket.types'
 import { ErrorCodes, ErrorDescriptionsArray } from '../../common/statusCodes'
-import { ProductService } from '../product/product.service'
-import { AccountService } from '../account/account.service'
 
 @Injectable()
 export class BasketService {
   constructor(
     @InjectRepository(Basket)
     private readonly basketRepository: Repository<Basket>,
-    // private readonly productService: ProductService,
-    // private readonly accountService: AccountService,
   ) {
   }
 
@@ -36,16 +32,6 @@ export class BasketService {
     return basket
   }
 
-  // async checkAccountExists(accountId: number): Promise<boolean> {
-  //   const account = await this.accountService.findOne(accountId)
-  //   return !!account // Повертає true, якщо запис знайдено, або false, якщо запис не знайдено
-  // }
-  //
-  // async checkProductExists(productId: number): Promise<boolean> {
-  //   const product = await this.productService.findOne(productId)
-  //   return !!product
-  // }
-
   async create(input: BasketAddInput) {
     const { accountId, productId, ...basketData } = input
 
@@ -53,16 +39,20 @@ export class BasketService {
       console.log(ErrorDescriptionsArray[ErrorCodes.IdNullError])
       throw new BadRequestException(ErrorCodes.IdNullError) //id були null
     }
-    // if (!(await this.checkAccountExists(accountId)) || !(await this.checkProductExists(productId))) {
-    //   console.log(ErrorDescriptionsArray[ErrorCodes.RelationshipError])
-    //   throw new BadRequestException(ErrorCodes.RelationshipError) //id були null
-    // }
+
     await this.IsProductInBasket(accountId, productId)
 
     const basket = new Basket(basketData)
     basket.account = Promise.resolve({ id: accountId } as Account)
     basket.product = Promise.resolve({ id: productId } as Product)
-    return await this.basketRepository.save(basket)
+
+    try {
+      return await this.basketRepository.save(basket)
+    } catch (e) {
+      //console.log(e)
+      console.log(ErrorDescriptionsArray[ErrorCodes.RelationshipError])
+      throw new BadRequestException(ErrorCodes.RelationshipError)
+    }
   }
 
   async update(id: number, input: BasketEditInput) {
