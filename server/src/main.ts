9743106ae86110a8c5bcf1a2db65731db3c34de5
aleksandr.shumskyi <aleksandr.shumskyi@4k.com.ua>
2023-09-02 18:11:04 +0300

    cors update

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ import { EntityNotFoundErrorFilter } from './common/filter/entity-not-found-erro
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {cors: true})
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalFilters(new EntityNotFoundErrorFilter())
   // app.setGlobalPrefix('api')
